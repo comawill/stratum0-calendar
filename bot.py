a0fbd7c6b873fb105dda1d450f6276f5ed9063fb
Sebastian Willenborg <comawill@wlnbrg.de>
2014-07-24 00:35:44 +0200

    little cleanup

--- bot.py
+++ bot.py
@@ -15,12 +15,12 @@ entries = calendergenerator.parse_wiki_page(termine.edit())
 
 old = hauptseite.edit(section=2)
 text = calendergenerator.generate_wiki_section(entries)
-#exit()
+
 if old != text:
 	rev = termine.revisions(limit = 1, prop = 'timestamp|user|comment').next()
 	changed = datetime.datetime.fromtimestamp(time.mktime(rev["timestamp"]))
 	now = datetime.datetime.utcnow()
-	comment = u"Automatisches Update (irgendwas wird sich schon verändert haben)"#.encode("utf8")
+	comment = u"Automatisches Update (irgendwas wird sich schon verändert haben)"
 	if now-changed < datetime.timedelta(minutes=15):
 		comment = u"%s hat Termine aktualisiert (%s) " % (rev["user"] ,rev["comment"])
 	print "update!"
