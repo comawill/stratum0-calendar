9a6e616e155f8bff3839d02ab5b60ed751c2f037
Sebastian Willenborg <comawill@wlnbrg.de>
2014-08-29 10:18:11 +0200

    gen_ical.py writes its output now also into a file

--- gen_ical.py
+++ gen_ical.py
@@ -3,19 +3,25 @@ import icalendar as ical
 import re
 import calendergenerator
 import mwclient
+import sys
 
+if __name__ == "__main__":
+	cal = ical.Calendar()
+	cal.add('version', '2.0')
+	cal.add('prodid', '-//willenbot')
+	cal.add('x-wr-calname', 'Stratum 0')
 
-cal = ical.Calendar()
-cal.add('version', '2.0')
-cal.add('prodid', '-//willenbot')
-cal.add('x-wr-calname', 'Stratum 0')
+	site = mwclient.Site(('https', 'stratum0.org'), path="/mediawiki/")
+	termine = site.Pages["Termine"]
+		
+	entries = calendergenerator.parse_wiki_page(termine.edit())
 
-site = mwclient.Site(('https', 'stratum0.org'), path="/mediawiki/")
-termine = site.Pages["Termine"]
-	
-entries = calendergenerator.parse_wiki_page(termine.edit())
+	for entry in entries:
+		cal.add_component(entry.getIcal())
 
-for entry in entries:
-	cal.add_component(entry.getIcal())
-
-print cal.to_ical()
+	if len(sys.argv) > 1:
+		f = file(sys.argv[1], "w")
+		f.write(cal.to_ical())
+		f.close()
+	else:
+		print cal.to_ical()
